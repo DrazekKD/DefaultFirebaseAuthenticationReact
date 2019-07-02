8b85cdb9bc3468d952972be454ad464467a57332
Kamil Drazkiewicz <k.drazkiewicz@getprintbox.com>
2019-07-02 12:39:19 +0200

    Firebase  Auth password and email login

--- react-firebase-authentication/src/components/SignUp/index.js
+++ react-firebase-authentication/src/components/SignUp/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import  * as ROUTES from '../../constants/routes'
+import { FirebaseContext } from '../Firebase';
 
-const SignUp = () => (
-				  <div>
-				  <h1>SignUp</h1>
-				  </div>
+import SignUpForm from './SignUpForm';
+
+const SignUpPage = () => (
+	<div>
+		<h1>SingUp</h1>
+		<FirebaseContext.Consumer>
+			{firebase => <SignUpForm firebase={firebase}/>}
+		</FirebaseContext.Consumer>
+	</div>
 );
 
-export default SignUp;
+const SignUpLink = () => (
+	<p>
+		Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+	</p>
+);
+export default SignUpPage;
+
+export {SignUpForm, SignUpLink}
